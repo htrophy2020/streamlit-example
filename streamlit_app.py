6cf58d3554cae5e5f79985241abed04d346af4a7
htrophy2020 <44591109+htrophy2020@users.noreply.github.com>
2022-02-25 21:58:52 -0500

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@
 import streamlit as st
 from datetime import date
 import yfinance as yf
-from fbprophet import prophet
+from fbprophet import Prophet
 from fbprophet.plot import plot_plotly
 from plotly import graph_objs as go
 
