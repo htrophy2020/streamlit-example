c516743dbdab5ac07a2226f7c4d381157b14052b
htrophy2020 <44591109+htrophy2020@users.noreply.github.com>
2022-02-25 20:45:58 -0500

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,70 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+# ai_fbprophet model
+
+# stock prediction - streamlit app
+
 import streamlit as st
-
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+from datetime import date
+import yfinance as yf
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly
+from plotly import graph_objs as go
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+START = "2015-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+st.title(Option Time Waves - Future")
 
-    points_per_turn = total_points / num_turns
+stocks = ("AAPL","GOOG","MSFT","XOM","INTC","IBM","MAR","CSCO")
+selected_stocks = st.selectbox(Select dataset for prediction", stocks)
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+n_years = st.slider("Years of prediction", 1, 4)
+period = n_years * 365
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+@st.cache
+def load_data(ticker):
+	data = yf.download(ticker, START, TODAY)
+	data.reset_index(inplace=True)
+	return data
+
+data_load_state = st.text("Load data...")
+data = load_data(selected_stock)
+data_load_state.text("Loading data...done!")
+
+st.subheader('Raw data')
+st.write(data.tail())
+
+def plow_raw_data():
+	fig = go.Figure()
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name='stock_open'))
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name='stock_close'))
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['High'], name='stock_high'))
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Low'], name='stock_low'))
+	fig.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True)
+	st.plotly_chart(fig)
+
+plot_raw_data()
+
+
+# Forcasting
+# need to look up symbol ref for additional items open,high,low
+
+df_train = data['Date','Close']
+df_train = df_train.rename(columns={"Date":"ds", "Close":"y"})
+
+m = Prophet()
+m.fit(df_train)
+future = m.make_future_dataframe(periods=period)
+forecast = m.predict(future)
+
+st.subheader('Forecast data')
+st.write(forecast.tail())
+
+st.write('forecast data')
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
+
+st.write('forecast components')
+fig2 = m.plot_components(forecast)
+st.write(fig2)
+
